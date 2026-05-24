49053056947b97ca5e8bf445cff4f0a563e043f4
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-05-24 20:46:11 -0300

    commits

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import ClientesModel
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-def criar_cliente
+def criar_cliente(request):
+    nome_cliente = ClientesModel.objects.create()
+    return HttpResponse ('cliente criado!')
