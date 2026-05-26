8ffcad439374b84b0bdf53537751e0ea37111840
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-05-26 20:19:24 -0300

    commits

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from clientes.models import ClientesModel
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class CardapioModel(models.Model):
+    cliente_cardapio = models.ForeignKey(ClientesModel, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    prato = models.CharField(max_length=1000)
+    detalhes_pedido = models.CharField(validators=[MinLengthValidator(1)])
+
+    def __str__ (self):
+        return self.prato
+
--- cardapio/views.py
+++ cardapio/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from cardapio.models import CardapioModel
+from django.http import HttpResponse
 
-# Create your views here.
+def criar_pedido(request, id):
+    pedido = CardapioModel.objects.create(id=id)
+    if request.method == 'POST':
+        return pedido    
+    return HttpResponse('Pedido criado ou existente!')
+
+
+def visualizar_pedido(request, id):
+    pedido = CardapioModel.objects.get(id)
+    if request.method == 'GET':
+        return pedido
+    return HttpResponse(f'Pedido do id: {id}')
+
+
+def deletar_pedido(request, id):
+    if request.method == 'DELETE':
+        pedido = CardapioModel.objects.get(id)
+        
+
+    
